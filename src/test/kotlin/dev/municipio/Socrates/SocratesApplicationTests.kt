7fd3b4bc6c80d9f23c66094566799831eb03be7a
Paulo Enoque Safrao <pauloenoque2014@gmail.com>
2026-01-17 20:44:55 +0100

    Spring project created

---
+++ src/test/kotlin/dev/municipio/Socrates/SocratesApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.municipio.Socrates
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocratesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
